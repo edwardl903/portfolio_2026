31bbdaee6494e2c23648deb42a048d20bd056643
Edward Lai <edwardlai@Edwards-MacBook-Pro.local>
2026-03-12 22:49:22 -0400

    Optimized images

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="container">
-        <p>&copy; 2026 Edward Lai. Built with ❤️ and lots of ☕</p>
+        <p>&copy; 2026 Edward Lai. Built with love and lots of coffee ☕</p>
       </div>
     </footer>
   )
